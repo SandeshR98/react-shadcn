91b97054c0c02791f1e5a24739113c4dd538e385
Sandesh Rathnayake <sandeshrathnayake@gmail.com>
2025-01-03 09:17:17 +0530

    feat(Login): sample login and logout added

--- react-shadcn-project/src/features/auth/login/forms/LoginForm.tsx
+++ react-shadcn-project/src/features/auth/login/forms/LoginForm.tsx
@@ -3,8 +3,11 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useNavigate } from 'react-router';
 
 export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
+	const navigate = useNavigate();
+
 	return (
 		<div className={cn('flex flex-col gap-6', className)} {...props}>
 			<Card>
@@ -13,7 +16,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
 					<CardDescription>Login with your Apple or Google account</CardDescription>
 				</CardHeader>
 				<CardContent>
-					<form>
+					<form onSubmit={() => navigate('/dashboard', { replace: true })}>
 						<div className='grid gap-6'>
 							<div className='flex flex-col gap-4'>
 								<Button variant='outline' className='w-full'>
@@ -41,7 +44,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
 							<div className='grid gap-6'>
 								<div className='grid gap-2'>
 									<Label htmlFor='email'>Email</Label>
-									<Input id='email' type='email' placeholder='m@example.com' required />
+									<Input id='email' type='email' placeholder='m@example.com' />
 								</div>
 								<div className='grid gap-2'>
 									<div className='flex items-center'>
@@ -50,7 +53,7 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
 											Forgot your password?
 										</a>
 									</div>
-									<Input id='password' type='password' required />
+									<Input id='password' type='password' />
 								</div>
 								<Button type='submit' className='w-full'>
 									Login
